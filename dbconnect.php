53d6d1f8133c9e07e7a607326ad62e5bf748dd88
RahmadMy <149589309+RahmadMy@users.noreply.github.com>
2026-01-14 21:40:05 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
-$host = "sql200.infinityfree.com";
-$user = "if0_40901151";
-$pass = "e38y1qR3Jv1";
-$db   = "if0_40901151_myonlineshopdatabase";
-
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect(
+  getenv("MYSQLHOST"),
+  getenv("MYSQLUSER"),
+  getenv("MYSQLPASSWORD"),
+  getenv("MYSQLDATABASE"),
+  getenv("MYSQLPORT")
+);
 
 if (!$conn) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+  die(json_encode(["error" => "DB connection failed"]));
 }
-?>
