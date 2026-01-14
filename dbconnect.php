c8052873a8c734c7a207b0edc7265091a31b844c
RahmadMy <149589309+RahmadMy@users.noreply.github.com>
2026-01-15 00:20:55 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = getenv("MYSQLHOST") ?: 'localhost';
+$db   = getenv("MYSQLDATABASE") ?: 'test';
+$port = getenv("MYSQLPORT") ?: 3306;
+$user = getenv("MYSQLUSER") ?: 'root';
+$pass = getenv("MYSQLPASSWORD") ?: '';
+
 try {
-    $conn = new PDO(
-        "mysql:host=" . getenv("MYSQLHOST") .
-        ";dbname=" . getenv("MYSQLDATABASE") .
-        ";port=" . getenv("MYSQLPORT") .
-        ";charset=utf8mb4",
-        getenv("MYSQLUSER"),
-        getenv("MYSQLPASSWORD"),
+    // Buat koneksi PDO
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;port=$port;charset=utf8mb4",
+        $user,
+        $pass,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
