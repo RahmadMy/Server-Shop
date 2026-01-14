096eee3204e82d26798b66d3a1ef3d741f9abf20
RahmadMy <149589309+RahmadMy@users.noreply.github.com>
2026-01-14 23:56:14 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,5 @@
 <?php
-if (defined('PDO::ATTR_DRIVER_NAME')) {
-    $drivers = PDO::getAvailableDrivers();
-    if (in_array('mysql', $drivers)) {
-        echo "PDO MySQL aktif ✅";
-    } else {
-        echo "PDO aktif tapi MySQL driver tidak ditemukan ❌";
-    }
-} else {
-    echo "PDO tidak aktif ❌";
-}
+$drivers = PDO::getAvailableDrivers();
+echo "Drivers aktif: ";
+print_r($drivers);
 ?>
